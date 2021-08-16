5ece00b480f62e395b7ad5d7c63c1c43dac55d76
SourabhGawade <42259132+SourabhGawade@users.noreply.github.com>
2021-08-16 08:34:10 +0530

    Update DBMain.java
    Product stud = session.get(Product.class, 123); is changed to Product prodData = session.get(Product.class, 123);

--- Databasehibernate/src/com/sourabh/DBMain.java
+++ Databasehibernate/src/com/sourabh/DBMain.java
@@ -10,22 +10,25 @@ public class DBMain {
 
 	public static void main(String[] args) {
 		
-		Product prod1 = new Product(1, "Saving", new Date());
+		//Product prod1 = new Product(123, "Saving", new Date());
 		
 		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Product.class)
 				.buildSessionFactory();
 
 		Session session = factory.getCurrentSession();
 		try {
+			
+			//Inserting data in DB using Hibernate
 			/*session.beginTransaction();
 			
 			System.out.println("\nSaving Prod.....");
 			session.save(prod1);
 			session.getTransaction().commit();*/
 			
+			//Retriving data from DB using Hibernate
 			session.beginTransaction();
-			Product stud = session.get(Product.class, 123);
-			System.out.println("\n\nRetrived Student :  " + stud.getName()+ stud.getNum());
+			Product prodData = session.get(Product.class, 123);
+			System.out.println("\n\nRetrived Student :  " + prodData.getName()+ prodData.getNum());
 			session.getTransaction().commit();
 			System.out.println("Done..");
 			
